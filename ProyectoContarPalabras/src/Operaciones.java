c1ed8606b7a046cb6586d4a41d601914417261d9
Ángel "Kade" García <contacto.kade.angel@gmail.com>
2020-03-26 23:12:16 +0100

    Javadoc creado para la clase Operaciones

--- ProyectoContarPalabras/src/Operaciones.java
+++ ProyectoContarPalabras/src/Operaciones.java
@@ -9,6 +9,13 @@
  * @author angel
  */
 public class Operaciones {
+    /**
+     * Cuenta el número de caracteres que tiene la frase introducida
+     * por el usuario
+     * @param cadena String con la frase que introduce el usuario
+     * @return cantidadCaracteres (int con el numero de caracteres de la frase
+     * introducida por el usuario)
+     */
     static int contarCaracteresTotal(String cadena){
         //devuelve el número total de caracteres dada una determinada cadena
         int cantidadCaracteres = 0;
@@ -21,6 +28,13 @@ public class Operaciones {
         return cantidadCaracteres;
     }
     
+    /**
+     * Cuenta el numero de palabras que contiene la frase introducida por
+     * el usuario
+     * @param cadena String con la frase que introduce el usuario
+     * @return cantidadPalabras (int con el numero de palabras que contiene
+     * la frase introducida por el usuario)
+     */
     static int contarNumeroPalabras(String cadena){
         //cuenta el número de palabras introducido
         int cantidadPalabras = 1;
@@ -34,10 +48,18 @@ public class Operaciones {
         
     }
     
+    /**
+     * Cuenta el numero de caracteres de cada tipo que contiene la frase
+     * introducida por el usuario
+     * @param cadena String con la frase que introduce el usuario
+     * @return contador (Array con la misma longitud que el array letras
+     * y que contiene para cada posición, el numero de caracteres que contiene
+     * la frase del caracter colocado en la misma posicion en el array "letras"
+     */
     static int[] contarTodosCaracteres(String cadena){
         //devuelve la cantidad de caracteres de cada tipo
-        InicioApp.letras = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-            'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's',
+        InicioApp.letras = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+            'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's',
             't', 'u', 'v', 'w', 'x', 'y', 'z', '.', ',', '?',
             '¿', '!', '¡', ';', ':', '(', ')'};
         int[] contador = new int[37];

82a8b260b14f6ffde2f8b68942fbe95599036fd5
Ángel "Kade" García <contacto.kade.angel@gmail.com>
2020-03-26 23:21:38 +0100

    Javadoc creado para la clase Resultados

--- ProyectoContarPalabras/src/Resultados.java
+++ ProyectoContarPalabras/src/Resultados.java
@@ -9,16 +9,34 @@
  * @author angel
  */
 public class Resultados {
+    /**
+     * @param total valor que devuelve el método
+     * Operaciones.contarCaracteresTotal()
+     * @return Un String con la cantidad de caracteres total que posee la frase
+     * introducida por el usuario, listo para imprimir por pantalla
+     */
     static String textoCaracteresTotal (int total){
         
         return "Cantidad de caracteres: " + total;
     }
     
+    /**
+     * @param total valor que devuelve el método
+     * Operaciones.contarNumeroPalabras()
+     * @return Un String con la cantidad de palabras que posee la frase 
+     * introducida por el usuario, listo para imprimir por pantalla
+     */
     static String textoNumeroPalabras (int total){
         
         return "Cantidad de palabras: " + total;
     }
     
+    /**
+     * @param contador Array que devuelve el método
+     * Operaciones.contarTodosCaracteres()
+     * @return Un String con la cantidad de veces que sale cada caracter en
+     * la frase introducida por el usuario, listo para imprimir por pantalla
+     */
     static String textoTodosCaracteres (int[] contador){
         String resultado = "";
         for (int i = 0; i < contador.length; i++) {

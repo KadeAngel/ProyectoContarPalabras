3a9f3de4cf223960da36e55ca3efddc09c2b6a5f
Ángel "Kade" García <contacto.kade.angel@gmail.com>
2020-03-26 22:25:41 +0100

    Realizados cambios en InicioApp, modularizando y dejando de esa forma en el main solo las llamadas a los métodos menu() y operacionesResultados().

--- ProyectoContarPalabras/src/InicioApp.java
+++ ProyectoContarPalabras/src/InicioApp.java
@@ -3,20 +3,30 @@ import java.util.Scanner;
 public class InicioApp {
 
     public static Scanner sc = new Scanner(System.in);
+    
+    public static String cadena;
 
     static char[] letras;
 
     public static void main(String[] args) {
 
+        menu();
+
+        operacionesResultados();
+    }
+    
+    static void menu () {
         System.out.println("------- Ejercicio cadena caracteres Array -------");
 
         System.out.println("Introduce una frase: ");
         String frase = sc.nextLine();
-        String cadena = frase.toLowerCase();//frase pero en minúsculas.
-
+        cadena = frase.toLowerCase();//frase pero en minúsculas.
+    }
+    
+    static void operacionesResultados(){
         System.out.println(Resultados.textoCaracteresTotal(Operaciones.contarCaracteresTotal(cadena)));
         System.out.println(Resultados.textoNumeroPalabras(Operaciones.contarNumeroPalabras(cadena)));
         System.out.println(Resultados.textoTodosCaracteres(Operaciones.contarTodosCaracteres(cadena)));
-
     }
+    
 }

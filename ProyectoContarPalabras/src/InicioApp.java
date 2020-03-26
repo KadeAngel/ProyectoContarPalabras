dce0f53141dbca8356f16d0079ff69b99cc646a7
Ángel "Kade" García <contacto.kade.angel@gmail.com>
2020-03-26 23:04:55 +0100

    Javadoc creado para la clase InicioApp

--- ProyectoContarPalabras/src/InicioApp.java
+++ ProyectoContarPalabras/src/InicioApp.java
@@ -1,13 +1,28 @@
 import java.util.Scanner;
 
+/**
+ *
+ * @author angel
+ */
 public class InicioApp {
 
+    /**
+     * Creacion de un objeto de la clase Scanner que permite pedir datos al
+     * usuario
+     */
     public static Scanner sc = new Scanner(System.in);
     
+    /**
+     *
+     */
     public static String cadena;
 
     static char[] letras;
 
+    /**
+     * Metodo main, en el cual ejecuto todo el programa
+     * @param args
+     */
     public static void main(String[] args) {
 
         menu();
@@ -15,6 +30,9 @@ public class InicioApp {
         operacionesResultados();
     }
     
+    /**
+     * Metodo en el que recibo la frase introducida por el usuario
+     */
     static void menu () {
         System.out.println("------- Ejercicio cadena caracteres Array -------");
 
@@ -23,6 +41,9 @@ public class InicioApp {
         cadena = frase.toLowerCase();//frase pero en minúsculas.
     }
     
+    /**
+     * Metodo que recoge la ejecucion de todos los metodos del proyecto
+     */
     static void operacionesResultados(){
         System.out.println(Resultados.textoCaracteresTotal(Operaciones.contarCaracteresTotal(cadena)));
         System.out.println(Resultados.textoNumeroPalabras(Operaciones.contarNumeroPalabras(cadena)));

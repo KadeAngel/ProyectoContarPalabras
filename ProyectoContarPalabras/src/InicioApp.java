6367d7fc3469a7470b0ccfa937b4d7fb13cd44e5
Ángel "Kade" García <contacto.kade.angel@gmail.com>
2020-03-26 22:12:46 +0100

    Creada la clase Operaciones, trasladados a esa clase los métodos que operaban con la frase del usuario y eliminados esos mismos métodos de la clase InicioApp, pues ya están en Operaciones. Cambiadas las referencias a esos métodos en la clase InicioApp

--- ProyectoContarPalabras/src/InicioApp.java
+++ ProyectoContarPalabras/src/InicioApp.java
@@ -14,54 +14,10 @@ public class InicioApp {
         String frase = sc.nextLine();
         String cadena = frase.toLowerCase();//frase pero en minúsculas.
 
-        System.out.println(textoCaracteresTotal(contarCaracteresTotal(cadena)));
-        System.out.println(textoNumeroPalabras(contarNumeroPalabras(cadena)));
-        System.out.println(textoTodosCaracteres(contarTodosCaracteres(cadena)));
+        System.out.println(textoCaracteresTotal(Operaciones.contarCaracteresTotal(cadena)));
+        System.out.println(textoNumeroPalabras(Operaciones.contarNumeroPalabras(cadena)));
+        System.out.println(textoTodosCaracteres(Operaciones.contarTodosCaracteres(cadena)));
 
-    }
-
-    static int contarCaracteresTotal(String cadena){
-        //devuelve el número total de caracteres dada una determinada cadena
-        int cantidadCaracteres = 0;
-        
-        for (int i = 0; i < cadena.length(); i++) {
-            if (cadena.charAt(i) != ' ') {
-                cantidadCaracteres++;
-            }
-        }
-        return cantidadCaracteres;
-    }
-    
-    static int contarNumeroPalabras(String cadena){
-        //cuenta el número de palabras introducido
-        int cantidadPalabras = 1;
-        
-        for (int i = 0; i < cadena.length(); i++) {
-            if (cadena.charAt(i) == ' ' && cadena.charAt(i - 1) != ' ') {
-                cantidadPalabras++;
-            }
-        }
-        return cantidadPalabras;
-        
-    }
-    
-    static int[] contarTodosCaracteres(String cadena){
-        //devuelve la cantidad de caracteres de cada tipo
-        letras = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-            'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's',
-            't', 'u', 'v', 'w', 'x', 'y', 'z', '.', ',', '?',
-            '¿', '!', '¡', ';', ':', '(', ')'};
-        int[] contador = new int[37];
-        for (int i = 0; i < cadena.length(); i++) {
-
-            for (int j = 0; j < contador.length; j++) {
-                if (cadena.charAt(i) == letras[j]) {
-                    contador[j]++;
-                }
-            }
-        }
-        return contador;
-        
     }
     
     static String textoCaracteresTotal (int total){
---
+++ ProyectoContarPalabras/src/Operaciones.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author angel
+ */
+public class Operaciones {
+    static int contarCaracteresTotal(String cadena){
+        //devuelve el número total de caracteres dada una determinada cadena
+        int cantidadCaracteres = 0;
+        
+        for (int i = 0; i < cadena.length(); i++) {
+            if (cadena.charAt(i) != ' ') {
+                cantidadCaracteres++;
+            }
+        }
+        return cantidadCaracteres;
+    }
+    
+    static int contarNumeroPalabras(String cadena){
+        //cuenta el número de palabras introducido
+        int cantidadPalabras = 1;
+        
+        for (int i = 0; i < cadena.length(); i++) {
+            if (cadena.charAt(i) == ' ' && cadena.charAt(i - 1) != ' ') {
+                cantidadPalabras++;
+            }
+        }
+        return cantidadPalabras;
+        
+    }
+    
+    static int[] contarTodosCaracteres(String cadena){
+        //devuelve la cantidad de caracteres de cada tipo
+        InicioApp.letras = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+            'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's',
+            't', 'u', 'v', 'w', 'x', 'y', 'z', '.', ',', '?',
+            '¿', '!', '¡', ';', ':', '(', ')'};
+        int[] contador = new int[37];
+        for (int i = 0; i < cadena.length(); i++) {
+
+            for (int j = 0; j < contador.length; j++) {
+                if (cadena.charAt(i) == InicioApp.letras[j]) {
+                    contador[j]++;
+                }
+            }
+        }
+        return contador;
+        
+    }
+}
